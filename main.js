a4c4b86aae73ee1ad446412cdbbfe8f98896a6ae
Gitto Hubbard <gnewsgroup@gmail.com>
2016-05-01 12:17:38 -0400

    Updated files by following the bootstrap standards, that is, wrap up page contents inside div class row and div class col-*-n divs. Also, beautified code using brackets' beautify extension.

--- main.js
+++ main.js
@@ -2,54 +2,54 @@
 // create the module and name it app
 var app = angular.module("mainApp", ['ngRoute']);
 
-app.config(function($routeProvider, $locationProvider){
-	$routeProvider
-	.when("/", {
-		templateUrl: "home.html",
-		controller: "mainController"
-	})
-	.when("/services", {
-		templateUrl: "services.html",
-		controller: "servicesController"
-	})
-	.when("/careers", {
-		templateUrl: "careers.html",
-		controller: "clientsController"
-	})
-	.when("/about", {
-		templateUrl: "about.html",
-		controller: "aboutController"
-	})
-	.when("/contact", {
-		templateUrl: "contact.html",
-		controller: "contactController"
-	})
-	
-	//$locationProvider.html5Mode(true);
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "home.html",
+            controller: "mainController"
+        })
+        .when("/services", {
+            templateUrl: "services.html",
+            controller: "servicesController"
+        })
+        .when("/careers", {
+            templateUrl: "careers.html",
+            controller: "clientsController"
+        })
+        .when("/about", {
+            templateUrl: "about.html",
+            controller: "aboutController"
+        })
+        .when("/contact", {
+            templateUrl: "contact.html",
+            controller: "contactController"
+        })
+
+    //$locationProvider.html5Mode(true);
 });
 
 // create the controller and inject Angular's $scope
-app.controller("mainController", function($scope) {
+app.controller("mainController", function ($scope) {
     // create a message to display in our view
-    $scope.message = "test only!";	
+    $scope.message = "test only!";
 });
 
-app.controller("servicesController", function($scope) {
+app.controller("servicesController", function ($scope) {
     // create a message to display in our view
-    $scope.message = "services only!";	
+    $scope.message = "services only!";
 });
 
-app.controller("clientsController", function($scope) {
+app.controller("clientsController", function ($scope) {
     // create a message to display in our view
-    $scope.message = "clients only!";	
+    $scope.message = "clients only!";
 });
 
-app.controller("aboutController", function($scope) {
+app.controller("aboutController", function ($scope) {
     // create a message to display in our view
     $scope.message = "about only!";
 });
 
-app.controller("contactController", function($scope) {
+app.controller("contactController", function ($scope) {
     // create a message to display in our view
     $scope.message = "contact us only!";
 });
--- modules.js
+++ modules.js
@@ -1,58 +1,75 @@
    // 1. Define the first app.
    var app = angular.module("mainApp", []);
-   
-   app.controller("test", function($scope){
-    $scope.Hola = {};
-	$scope.Hola.fn = "jon";
-	$scope.Hola.ln = "doe";
+
+   app.controller("test", function ($scope) {
+       $scope.Hola = {};
+       $scope.Hola.fn = "jon";
+       $scope.Hola.ln = "doe";
    });
-   
-   app.config(["$routeProvider", function($routeProvider){
-	   $routeProvider.
-	   when("/link1", {
-		   templateUrl: "link1.html",
-		   controller: "Link1"
-	   }).
-	   when("/link2", {
-		   templateUrl: "link2.html",
-		   controller: "Link2"
-	   }).
-	   when("/link3", {
-		   templateUrl: "link3.html",
-		   controller: "Link3"
-	   });
-   }]   
-   );
-   
+
+   app.config(["$routeProvider", function ($routeProvider) {
+       $routeProvider.
+       when("/link1", {
+           templateUrl: "link1.html",
+           controller: "Link1"
+       }).
+       when("/link2", {
+           templateUrl: "link2.html",
+           controller: "Link2"
+       }).
+       when("/link3", {
+           templateUrl: "link3.html",
+           controller: "Link3"
+       });
+   }]);
+
    // 2. Define the second app.
    var emp = angular.module("empApp", []);
-   
-   emp.controller("employee", function ($scope){
-	$scope.employee = {
-	   firstName: "Matt",
-	   lastName: "Kidder",
-	   fullName: function(){
-		   //var obj; 
-		   //obj = $scope.employee;
-		   return this.firstName + " " + this.lastName;		   
-	   },
-	   skills: [
-	    {name: "C#", strength: 10},
-	    {name: "JavaScript", strength: 6},
-	    {name: "VB", strength: 1},
-	    {name: "jQuery", strength: 2},
-		{name: "T-SQL", strength: 5},
-		{name: "UI Design", strength: 8}
+
+   emp.controller("employee", function ($scope) {
+       $scope.employee = {
+           firstName: "Matt",
+           lastName: "Kidder",
+           fullName: function () {
+               //var obj; 
+               //obj = $scope.employee;
+               return this.firstName + " " + this.lastName;
+           },
+           skills: [
+               {
+                   name: "C#",
+                   strength: 10
+               },
+               {
+                   name: "JavaScript",
+                   strength: 6
+               },
+               {
+                   name: "VB",
+                   strength: 1
+               },
+               {
+                   name: "jQuery",
+                   strength: 2
+               },
+               {
+                   name: "T-SQL",
+                   strength: 5
+               },
+               {
+                   name: "UI Design",
+                   strength: 8
+               }
 	   ]
-	};   
+       };
    });
-   
+
    //Angular will always bootstrap the first app defined in the view. 
    //In this case, mainApp is already bootstrapped. So, do NOT manually
    //bootstrap it, or else exception will occur.   
    //var first = document.getElementById("first");   
    //angular.bootstrap(angular.element(first), ["mainApp"]);
-   
+
    //Do manually bootstrap the second app, because by default, angular will only
    //bootstrap the first app that appear in the view.
    var second = document.getElementById("second");
